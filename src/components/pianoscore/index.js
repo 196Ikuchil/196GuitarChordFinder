b72a6cfadeee3ff0b0083a8c43ce455d8091b0b3
196Ikuchil <196thinline@gmail.com>
2021-07-05 00:40:19 +0900

    feat:implement pianoscore rendering

---
+++ src/components/pianoscore/index.js
@@ -0,0 +1,1 @@
+export { default as PianoScore } from './react-abcjs';
---
+++ src/components/pianoscore/react-abcjs.js
@@ -0,0 +1,53 @@
+/* eslint-disable react/sort-comp */
+/* eslint-disable no-unused-vars */
+import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
+import abcjsObj from 'abcjs';
+
+class Abcjs extends PureComponent {
+  uniqueNumber = Date.now() + Math.random();
+
+  renderAbcNotation(abcNotation, parserParams, engraverParams, renderParams) {
+    const res = abcjsObj.renderAbc(
+      `abcjs-result-${this.uniqueNumber}`,
+      abcNotation,
+      parserParams,
+      engraverParams,
+      renderParams
+    );
+  }
+
+  componentDidMount() {
+    const { abcNotation, parserParams, engraverParams, renderParams } = this.props;
+    this.renderAbcNotation(abcNotation, parserParams, engraverParams, renderParams);
+  }
+
+  componentDidUpdate() {
+    const { abcNotation, parserParams, engraverParams, renderParams } = this.props;
+    this.renderAbcNotation(abcNotation, parserParams, engraverParams, renderParams);
+  }
+
+  render() {
+    return (
+      <div style={{ width: '100%' }}>
+        <div id={`abcjs-result-${this.uniqueNumber}`} style={{ width: '100%' }} />
+      </div>
+    );
+  }
+}
+
+Abcjs.propTypes = {
+  abcNotation: PropTypes.string,
+  parserParams: PropTypes.object,
+  engraverParams: PropTypes.object,
+  renderParams: PropTypes.object
+};
+
+Abcjs.defaultProps = {
+  abcNotation: '',
+  parserParams: {},
+  engraverParams: { responsive: 'resize' },
+  renderParams: { viewportHorizontal: true }
+};
+
+export default Abcjs;
