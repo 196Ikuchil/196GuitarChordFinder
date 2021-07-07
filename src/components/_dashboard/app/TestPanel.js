4303fd9a69a57b851ac5d7aef86ed9800d62cbab
196Ikuchil <196thinline@gmail.com>
2021-07-08 01:41:46 +0900

    feat:module test

--- src/components/_dashboard/app/TestPanel.js
+++ src/components/_dashboard/app/TestPanel.js
@@ -2,6 +2,7 @@
 import { Card, CardHeader, Box } from '@material-ui/core';
 import { ChordBox } from '../../vexchords';
 import { PianoBox } from '../../pianokeyboard';
+import { GetDiatonicChordNames } from '../../../utils/music';
 
 // ----------------------------------------------------------------------
 
@@ -11,6 +12,8 @@ export default function TestPanel() {
       <CardHeader title="Website Visits" subheader="(+43%) than last year" />
       <Box sx={{ p: 3, pb: 1 }} dir="ltr">
         <ChordBox />
+        {GetDiatonicChordNames(0, 0)}
+        {GetDiatonicChordNames(1, 1)}
         <PianoBox />
       </Box>
     </Card>
