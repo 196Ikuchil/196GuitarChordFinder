4b49200cd40319103e316d5aaf19d7189b9fa97d
196Ikuchil <196thinline@gmail.com>
2021-08-07 04:00:11 +0900

    feat:LightTooltips

--- src/components/LightTooltip.js
+++ src/components/LightTooltip.js
@@ -1,27 +1,56 @@
+import { useState } from 'react';
+import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/styles';
 import Tooltip from '@material-ui/core/Tooltip';
-import { Fade } from '@material-ui/core';
+import { Fade, Button } from '@material-ui/core';
 
 const StyledLightTooltip = withStyles((theme) => ({
   arrow: {
     color: theme.palette.info.lighter
   },
   tooltip: {
+    padding: 10,
     backgroundColor: theme.palette.info.lighter,
     color: 'rgba(30, 40, 40, 0.87)',
     boxShadow: theme.shadows[1],
-    fontSize: 16
+    fontSize: 14,
+    maxWidth: '22em',
+    [theme.breakpoints.down('sm')]: {
+      maxWidth: '10em'
+    }
   }
 }))(Tooltip);
 
-export const LightTooltip = (props) => (
-  <StyledLightTooltip
-    arrow
-    title={props.text}
-    TransitionComponent={Fade}
-    TransitionProps={{ timeout: 900 }}
-    {...props}
-  >
-    {props.children}
-  </StyledLightTooltip>
-);
+export const LightTooltip = (props) => {
+  const [open, setOpen] = useState(true);
+  const test = () => {
+    setOpen(false);
+  };
+
+  return (
+    <StyledLightTooltip
+      arrow
+      open={Boolean(props.condition) && open}
+      title={
+        <>
+          <center>
+            {props.text}
+            <br />
+            <Button onClick={() => setOpen(false)}>OK</Button>
+          </center>
+        </>
+      }
+      TransitionComponent={Fade}
+      TransitionProps={{ timeout: 900 }}
+      {...props}
+    >
+      {props.children}
+    </StyledLightTooltip>
+  );
+};
+
+LightTooltip.defaultProps = {
+  condition: PropTypes.number.isRequired,
+  text: PropTypes.string.isRequired,
+  children: PropTypes.element.isRequired
+};
